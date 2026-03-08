a6b779e9b4633038d5f9d6b52d4f1a6090105d52
Jaasiel-TR <jasieltorrero6@gmail.com>
2026-03-08 01:29:28 -0700

    se corrigio la clase Usuario

--- parcial-1-poo/practica-2/Usuario.php
+++ parcial-1-poo/practica-2/Usuario.php
@@ -1,23 +1,36 @@
 <?php
+
+// Esta clase representa a un usuario con nombre y correo
 class Usuario{
+
+    // Atributos privados de la clase
     private $nombre;
     private $correo;
 
+    // Constructor de la clase
+    // Recibe el nombre y el correo 
     public function __construct($nombre, $correo){
-        $this->nombre = $nombre;
-        $this->correo = $correo;
+        $this->nombre = $nombre;  
+        $this->correo = $correo;   
     }
+
+    // Método getter para obtener el nombre del usuario
     public function getNombre(){
         return $this->nombre;
     }
+
+    // Método getter para obtener el correo del usuario
     public function getCorreo(){
         return $this->correo;
     }
+
+    // Método setter para cambiar el nombre del usuario
     public function setNombre($nombre){
-        return $this->nombre = $nombre;
+        $this->nombre = $nombre;
     }
-    public function setNorreo($correo){
-        return $this->correo = $correo;
+
+    // Método setter para cambiar el correo del usuario
+    public function setCorreo($correo){
+        $this->correo = $correo;
     }
 }
-?>
